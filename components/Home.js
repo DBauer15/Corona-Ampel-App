ea3b5d39cde53ff81cd02fdb742810ab1702cff7
David Bauer <davbauer@ucdavis.edu>
2020-09-23 02:12:54 +0200

    Error management, artwork, release config

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, Text, View, SafeAreaView, ScrollView, RefreshControl } from 'react-native';
+import { ActivityIndicator, Text, View, SafeAreaView, ScrollView, RefreshControl, Linking } from 'react-native';
 import * as Location from 'expo-location';
-import Constants from 'expo-constants';
+import ErrorMessages from '../utils'
 
 const SERVER_ADDRESS = 'http://18.188.89.203:8080/'
+//const SERVER_ADDRESS = 'http://192.168.1.100:5000/'
 
 class Home extends Component {
 
@@ -11,7 +12,8 @@ class Home extends Component {
     super()
     this.state = {
       refreshing: false,
-      status: 'loading',
+      status: 'LOADING',
+      status_code: '',
       location: '',
       info: '',
       style: {
@@ -25,31 +27,49 @@ class Home extends Component {
         },
         view: {
           flex: 1,
-          paddingLeft: 2,
-          paddingRight: 2,
+          paddingLeft: 30,
+          paddingRight: 30,
           backgroundColor: '#FAFAFA',
           alignItems: 'center',
           justifyContent: 'center'
         },
-        button: {
-          marginTop: 40
+        link: {
+          textAlign: 'center',
+          fontSize: 12,
+          color: '#333333',
+          opacity: 0.5,
+          marginTop: 20,
+          textDecorationLine: 'underline'
         },
         loading: {
           margin: 40
         },
+        textInfoHeading: {
+          textAlign: 'center',
+          fontWeight: '700',
+          fontSize: 28,
+          color: '#212121',
+          marginBottom: 25
+        },
+        textInfo: {
+          textAlign: 'center',
+          fontSize: 18,
+          color: '#666666',
+        },
         text: {
+          textAlign: 'center',
           fontSize: 24,
           color: '#333333',
           margin: 5,
         },
         textSmall: {
+          textAlign: 'center',
           fontSize: 12,
           color: '#333333',
           opacity: 0.5,
-          marginLeft: 20,
-          marginRight: 20,
         },
         textBig: {
+          textAlign: 'center',
           fontWeight: 'bold',
           fontSize: 32,
           color: '#333333',
@@ -65,7 +85,7 @@ class Home extends Component {
   }
 
   onRefresh() {
-    if (this.state.status === 'ok') {
+    if (this.state.status !== 'LOADING') {
       this.setState({ refreshing: true })
       this.update()
     }
@@ -75,7 +95,7 @@ class Home extends Component {
     Location.requestPermissionsAsync()
       .then(response => {
         if (response.status !== 'granted') {
-          this.setState({ status: 'noLocation' })
+          this.setState({ status: 'ERROR', status_code: 'NO_LOCATION', refreshing: false })
         } else {
           Location.getCurrentPositionAsync({})
             .then(response => {
@@ -85,6 +105,9 @@ class Home extends Component {
             })
         }
       })
+      .catch(_ => {
+        this.setState({ status: 'ERROR', status_code: 'NO_LOCATION', refreshing: false })
+      })
   }
 
   getAmpelInfo(lon, lat) {
@@ -92,57 +115,74 @@ class Home extends Component {
       .then(response => {
         if (response.status !== 200) {
           console.log('Looks like there was a problem. Status Code: ' + response.status)
-          return
-        }
-
-        response.json().then(data => {
-          this.setState({
-            status: 'ok',
-            refreshing: false,
-            location: data.postcode,
-            info: data.ampel_info.info,
-            time: data.stand * 1000,
-            style: {
-              ...this.state.style,
-              safeAreaView: { ...this.state.style.safeAreaView, backgroundColor: data.ampel_info.backgroundColor },
-              scrollView: { ...this.state.style.scrollView, backgroundColor: data.ampel_info.backgroundColor },
-              view: { ...this.state.style.view, backgroundColor: data.ampel_info.backgroundColor },
-              text: { ...this.state.style.text, color: data.ampel_info.color },
-              textSmall: { ...this.state.style.textSmall, color: data.ampel_info.color },
-              textBig: { ...this.state.style.textBig, color: data.ampel_info.color },
-            }
+          response.text().then(data => {
+            console.log(data)
+            this.setState({ 
+              status: 'ERROR', 
+              status_code: data,
+              refreshing: false,
+              style: {
+                ...this.state.style,
+                safeAreaView: { ...this.state.style.safeAreaView, backgroundColor: '#FAFAFA' },
+                scrollView: { ...this.state.style.scrollView, backgroundColor: '#FAFAFA' },
+                view: { ...this.state.style.view, backgroundColor: '#FAFAFA' },
+              }
+            })
           })
-        })
+        } else {
+          response.json().then(data => {
+            this.setState({
+              status: 'OK',
+              status_code: '',
+              refreshing: false,
+              location: data.postcode,
+              info: data.ampel_info.info,
+              time: data.stand * 1000,
+              style: {
+                ...this.state.style,
+                safeAreaView: { ...this.state.style.safeAreaView, backgroundColor: data.ampel_info.backgroundColor },
+                scrollView: { ...this.state.style.scrollView, backgroundColor: data.ampel_info.backgroundColor },
+                view: { ...this.state.style.view, backgroundColor: data.ampel_info.backgroundColor },
+                text: { ...this.state.style.text, color: data.ampel_info.color },
+                textSmall: { ...this.state.style.textSmall, color: data.ampel_info.color },
+                textBig: { ...this.state.style.textBig, color: data.ampel_info.color },
+              }
+            })
+          })
+        }
       })
       .catch(error => {
         console.log(error)
       })
+
   }
 
   render() {
     let contentView;
-    if (this.state.status === 'loading') {
+    if (this.state.status === 'LOADING') {
       contentView = (
         <View style={this.state.style.view}>
-          <Text style={this.state.style.text}>Bitte warten</Text>
-          <Text style={this.state.style.textSmall}>Erlaube der App Zugriff auf den Standort um Informationen zu deinem Bezirk zu erhalten.</Text>
+          <Text style={this.state.style.textInfoHeading}>Bitte warten</Text>
+          <Text style={this.state.style.textInfo}>Die Ampel wird für deinen Standort geschalten!</Text>
           <ActivityIndicator size='large' style={this.state.style.loading}></ActivityIndicator>
         </View>
       );
-    } else if (this.state.status === 'noLocation') {
+    } else if (this.state.status === 'ERROR') {
       contentView = (
         <View style={this.state.style.view}>
-          <Text style={this.state.style.text}>Kein Zugriff auf den Standort</Text>
-          <Text style={this.state.style.textSmall}>Stelle sicher dass die App Zugriff auf deinen Standort hat.</Text>
+          <Text style={this.state.style.textInfoHeading}>{ErrorMessages[this.state.status_code]['title']}</Text>
+          <Text style={this.state.style.textInfo}>{ErrorMessages[this.state.status_code]['description']}</Text>
         </View>
       );
-    } else if (this.state.status === 'ok') {
+    }
+    else if (this.state.status === 'OK') {
       contentView = (
         <View style={this.state.style.view}>
           <Text style={this.state.style.text}>An deinem Standort</Text>
           <Text style={this.state.style.textBig}>{this.state.info}</Text>
           <Text style={this.state.style.textSmall}>Basierend auf deiner PLZ {this.state.location}</Text>
           <Text style={this.state.style.textSmall}>Stand {(new Date(this.state.time)).toLocaleDateString()}</Text>
+          <Text style={this.state.style.link} onPress={ ()=> Linking.openURL('https://corona-ampel.gv.at') } >Hier mehr erfahren</Text>
         </View>
       );
     }
---
+++ utils.js
@@ -0,0 +1,24 @@
+const ErrorMessages = {
+    'TEST': {
+        'title': 'Fehler bei der Verbindung',
+        'description': 'Es ist ein Fehler bei der Verbindung mit dem Service aufgetreten. Bitte versuche es später nochmal.',
+    },
+    'INVALID_REQUEST': {
+        'title': 'Fehler bei der Verbindung',
+        'description': 'Es ist ein Fehler bei der Verbindung mit dem Service aufgetreten. Bitte versuche es später nochmal.',
+    },
+    'INVALID_COORD': {
+        'title': 'Fehler bei der Standortbestimmung',
+        'description': 'Wir konnten deinen Standort nicht bestimmen. Bitte versuche es später nochmal.',
+    },
+    'OUT_OF_REGION': {
+        'title': 'Keine Ampeldaten',
+        'description': 'In deiner Region stehen uns keine Ampeldaten zur Verfügung.',
+    },
+    'NO_LOCATION': {
+        'title': 'Kein Zugriff auf den Standort',
+        'description': 'Stelle sicher dass die App Zugriff auf deinen Standort hat.',
+    },
+}
+
+export default ErrorMessages
